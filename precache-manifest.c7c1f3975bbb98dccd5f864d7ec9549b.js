c4a8c1b777070a1736952070d32e15f2a7b079c5
DenisNikolski <dzianisnikolski@gmail.com>
2020-11-09 18:49:48 +0300

    Updates

---
+++ precache-manifest.c7c1f3975bbb98dccd5f864d7ec9549b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743016d6a3e7dccdecca856766df30b8",
+    "url": "/postsignal/index.html"
+  },
+  {
+    "revision": "a95c13187bac409254f5",
+    "url": "/postsignal/static/js/2.645ef46f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/postsignal/static/js/2.645ef46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46a4bc0e8da20defa09e",
+    "url": "/postsignal/static/js/main.91b29d4c.chunk.js"
+  },
+  {
+    "revision": "b46317abc554e3a64b4e",
+    "url": "/postsignal/static/js/runtime-main.f94ea8f1.js"
+  }
+]);
