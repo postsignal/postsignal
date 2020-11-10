75e5a955e0ac002ef4a7c2ce5fbeebe41cb638df
DenisNikolski <dzianisnikolski@gmail.com>
2020-11-11 01:06:09 +0300

    Updates

---
+++ precache-manifest.c8d3109d42c0da329ad5c81afabad000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9283df89e55520dbd293f0cce376c70e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea8f134760979d4ff455",
+    "url": "/static/js/2.f9a556d7.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.f9a556d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa726a441fdd0178573b",
+    "url": "/static/js/main.9c4e8866.chunk.js"
+  },
+  {
+    "revision": "044fa5866b762449e105",
+    "url": "/static/js/runtime-main.aae79b0d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/postsignal/precache-manifest.6e8c88bc58f4af3e2ac968a4dfa95e03.js"
+  "/precache-manifest.c8d3109d42c0da329ad5c81afabad000.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/postsignal/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
