9170d20f8c67c090ac6675c8792fa873f490fc81
DenisNikolski <dzianisnikolski@gmail.com>
2021-01-23 12:33:18 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.490bc37b9429927e1b0cd0bff25d97b6.js"
+  "/precache-manifest.8db7fa460bd702f8d84d9b5ac27ccad2.js"
 );
 
 self.addEventListener('message', (event) => {
